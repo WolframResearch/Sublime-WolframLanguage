63c2f6bc29dd77b705e8d6c6ed1b3f164ab7c80b
Brenton Bostick <brenton@wolfram.com>
2020-03-24 14:45:47 -0500

    Create a paclet for SublimeWolframLanguage for documentation

---
+++ SublimeWolframLanguage/PacletInfo.m
@@ -0,0 +1,10 @@
+
+Paclet[
+  Name -> "SublimeWolframLanguage",
+  Version -> "1.0",
+  WolframVersion -> "12.0+",
+  Creator ->"Brenton Bostick <brenton@wolfram.com>",
+  Extensions -> {
+    {"Documentation", Language -> "English", MainPage -> "Guides/SublimeWolframLanguage"}
+  }
+]
